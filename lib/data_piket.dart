a9f06f898a1469deb3881e26d1f3eac3b1b8db6a
rizkiagungdzulikromi <riskiagung3005@gmail.com>
2025-04-29 08:30:40 +0700

    Tambah tugas dan format tanggal

--- lib/data_piket.dart
+++ lib/data_piket.dart
@@ -8,3 +8,44 @@ class PiketGudangPage extends StatefulWidget {
   _PiketGudangPageState createState() => _PiketGudangPageState();
 }
 
+class _PiketGudangPageState extends State<PiketGudangPage> {
+  final TextEditingController _namaController = TextEditingController(text: 'Admin');
+  final TextEditingController _tugasController = TextEditingController();
+  DateTime? _selectedDate;
+
+  List<Map<String, dynamic>> tugasList = [];
+
+  bool _isTugasError = false;
+  bool _isTanggalError = false;
+
+  void _addTugas() {
+    setState(() {
+      _isTugasError = _tugasController.text.isEmpty;
+      _isTanggalError = _selectedDate == null;
+    });
+
+    if (!_isTugasError && !_isTanggalError) {
+      setState(() {
+        tugasList.add({
+          'nama': _namaController.text,
+          'tanggal': _selectedDate,
+          'tugas': _tugasController.text,
+        });
+        _tugasController.clear();
+        _selectedDate = null;
+        _isTugasError = false;
+        _isTanggalError = false;
+      });
+    }
+  }
+
+  String _formatTanggal(DateTime tanggal) {
+    final List<String> hari = ['Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu', 'Minggu'];
+    final List<String> bulan = [
+      'Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni',
+      'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'
+    ];
+    return '${hari[tanggal.weekday - 1]}, ${tanggal.day} ${bulan[tanggal.month - 1]} ${tanggal.year}';
+  }
+
+  

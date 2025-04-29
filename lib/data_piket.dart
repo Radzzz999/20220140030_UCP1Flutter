3a9320e6133818e7183b504808613e02df5536eb
rizkiagungdzulikromi <riskiagung3005@gmail.com>
2025-04-29 08:27:21 +0700

    membuat data piket dart

---
+++ lib/data_piket.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:ucp1/detail_piket';
+
+class PiketGudangPage extends StatefulWidget {
+  const PiketGudangPage({Key? key}) : super(key: key);
+
+  @override
+  _PiketGudangPageState createState() => _PiketGudangPageState();
+}
+

6a43598f4acc6776ff749a65ea0c5bdbfce953b7
rizkiagungdzulikromi <riskiagung3005@gmail.com>
2025-04-29 08:35:06 +0700

    Buat halaman detail piket gudang

---
+++ lib/detail_piket.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DetailPiketPage extends StatelessWidget {
+  final String nama;
+  final DateTime tanggal;
+  final String tugas;
+
+  const DetailPiketPage({
+    Key? key,
+    required this.nama,
+    required this.tanggal,
+    required this.tugas,
+  }) : super(key: key);
+
+  

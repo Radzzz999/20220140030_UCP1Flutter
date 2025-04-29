f6733a8bae041cc80dedc0c3dd7aaddfdb725995
rizkiagungdzulikromi <riskiagung3005@gmail.com>
2025-04-29 08:07:48 +0700

    membuat register dart

---
+++ lib/register.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({Key? key}) : super(key: key);
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+

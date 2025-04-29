7b88b1acfbf6b87a7145e84b635ccf920954d365
rizkiagungdzulikromi <riskiagung3005@gmail.com>
2025-04-29 08:21:16 +0700

    Menambahkan header dengan avatar dan tombol logout

--- lib/home.dart
+++ lib/home.dart
@@ -4,4 +4,62 @@ import 'package:flutter/services.dart';
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
-  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFFEF8F6),
+      body: Column(
+        children: [
+          
+          Container(
+            padding: const EdgeInsets.only(left: 20, top: 30, right: 20, bottom: 20),
+            color: Colors.redAccent.shade700,
+            child: Stack(
+              children: [
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    ClipOval(
+                      child: Image.asset(
+                        'assets/avatar.png',
+                        height: 70,
+                        width: 70,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    const SizedBox(width: 16),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const [
+                        Text(
+                          'Selamat Datang',
+                          style: TextStyle(color: Colors.white, fontSize: 16),
+                        ),
+                        SizedBox(height: 4),
+                        Text(
+                          'Admin',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 22,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                Positioned(
+                  right: 0,
+                  top: 0,
+                  child: IconButton(
+                    onPressed: () {
+                      SystemNavigator.pop(); 
+                    },
+                    icon: const Icon(Icons.logout, color: Colors.white),
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+          
